69ea0ea1e4e8b245fe0bd44a1146804751dc5cc4
benjamino4 <benjamindaniels1999@gmail.com>
2026-09-21 12:57:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ import httpx
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
-import psycopg2
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 USE_PG = DATABASE_URL.startswith("postgres")
